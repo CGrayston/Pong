3bc7d85806a793744021f2096087da49ec16b237
Christopher Grayston <christophergrayston@C02CD1CWMD6Rlocal.local>
2020-05-02 17:33:00 -0700

    Simple pong game in SpriteKit

---
+++ Pong/GameScene.swift
@@ -0,0 +1,87 @@
+//
+//  GameScene.swift
+//  Pong
+//
+//  Created by Christopher Grayston on 5/2/20.
+//  Copyright © 2020 Christopher Grayston. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var main = SKSpriteNode()
+    
+    var score = [Int]()
+    
+    override func didMove(to view: SKView) {
+        startGame()
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+        
+        // Make ball impulse
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        
+        // Make border along edge of screen
+        //let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        //let border =
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
+    }
+    
+    func startGame() {
+        score = [0,0]
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        } else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        print(score)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            // Grab location of finger inside of a view
+            let location = touch.location(in: self)
+            
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            // Grab location of finger inside of a view
+            let location = touch.location(in: self)
+            
+            main.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= main.position.y - 70 {
+            addScore(playerWhoWon: enemy)
+        } else if ball.position.y >= enemy.position.y + 70 {
+            addScore(playerWhoWon: main)
+        }
+    }
+}
